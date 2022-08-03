83063f51a35be7da68f1e72edcfb6eeb0ea048e6
George Boot <884482+georgeboot@users.noreply.github.com>
2022-08-03 16:43:41 +0200

    Add named export issue

---
+++ src/routes/arcgis.svelte
@@ -0,0 +1,19 @@
+<script>
+    import MapView from "@arcgis/core/views/MapView"
+    import "@arcgis/core/assets/esri/themes/light/main.css"
+
+    const createMap = (domNode) => {
+        view = new MapView({
+            container: domNode,
+            map: {
+                basemap: "topo-vector",
+            },
+            center: [5.25, 52.14],
+            zoom: 8,
+        })
+    }
+</script>
+
+<main>
+    <div use:createMap />
+</main>
